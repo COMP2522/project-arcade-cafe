ca5054ac63aa3e6276cefce61a547078b33bae72
Helen Liu <mail2helenl@gmail.com>
2023-03-14 16:53:45 -0700

    created Sprite class with variables x, y, color, size, window; added get and set methods for x, y, and size; added draw, move, and collided methods

---
+++ src/main/java/org/bcit/comp2522/project/Sprite.java
@@ -0,0 +1,54 @@
+package org.bcit.comp2522.project;
+
+import processing.core.PVector;
+
+import java.awt.*;
+
+public class Sprite {
+  protected int x;
+  protected int y;
+  protected float size;
+  protected Color color;
+  protected Window window;
+
+  public Sprite(int xPos, int yPos, int size, Color color, Window window) {
+    this.x = xPos;
+    this.y = yPos;
+    this.size = size;
+    this.color = color;
+    this.window = window;
+  }
+
+  public int getX() {
+    return this.x;
+  }
+  public void setX(int xPos) {
+    this.x = xPos;
+  }
+  public int getY() {
+    return this.y;
+  }
+  public void setY(int yPos) {
+    this.y = yPos;
+  }
+  public float getSize() {
+    return size;
+  }
+  public void draw() {
+    window.pushStyle();
+    window.fill(this.color.getRed(), this.color.getGreen(), this.color.getBlue());
+    window.ellipse(this.x, this.y, size, size);
+    window.popStyle();
+  }
+  public void move(int dx, int dy) {
+    this.x += dx;
+    this.y += dy;
+  }
+  public static boolean collided(Sprite a, Sprite b) {
+    float distance = (float) Math.sqrt((b.getX() - a.getX())^2 - (b.getY() - a.getY())^2);
+    if (distance <= (a.getSize() + b.getSize())) {
+      return true;
+    }
+    return false;
+  }
+}
