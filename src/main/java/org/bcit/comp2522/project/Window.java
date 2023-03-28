f2827d117623b60b365094733c07548585e99804
Mylo <emdoubleu.yu@gmail.com>
2023-03-27 22:30:42 -0700

    made a pause button, tweaked powerup manager

--- src/main/java/org/bcit/comp2522/project/Window.java
+++ src/main/java/org/bcit/comp2522/project/Window.java
@@ -14,7 +14,7 @@ public class Window extends PApplet {
 
   public boolean leftPressed = false;
   public boolean rightPressed = false;
-
+  public boolean wasPaused = false;
 
   public void settings() {
     size(960, 540);
@@ -25,7 +25,7 @@ public class Window extends PApplet {
 
     BulletManager.getInstance(this);
     EnemyManager.getInstance(this);
-    PowerUpManager.getInstance(5, 300, this); // Adjust spawnTime and spawnArea as needed
+    PowerUpManager.getInstance(200, width*4/5, this); // Adjust spawnTime and spawnArea as needed
     //TODO: tweak to find a good amount of HP and Firerate once we got a game going
     Player.getInstance(500, 490, 20, new Color(255, 255, 0), this,5,6);
     lm = LevelManager.getInstance();
@@ -66,6 +66,10 @@ public class Window extends PApplet {
 
   @Override
   public void keyPressed() {
+    if(key == ' ' && !wasPaused) {
+      lm.pause();
+      wasPaused = true;
+    }
     if(key == CODED) {
       if(keyCode == LEFT) {
         leftPressed = true;
@@ -77,6 +81,9 @@ public class Window extends PApplet {
   }
   @Override
   public void keyReleased() {
+    if(key == ' ') {
+      wasPaused = false;
+    }
     if(key == CODED) {
       if(keyCode == LEFT) {
         leftPressed = false;
