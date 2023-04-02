d0b39ea1272a1eb260393c48bc0479311b2f5770
Eric Cho <81988553+chosm7489@users.noreply.github.com>
2023-04-02 12:50:12 -0700

    added scoreboardmenu, gameovermenu, menuManager and refacotred window (#136)

---
+++ src/main/java/org/bcit/comp2522/project/ScoreboardMenu.java
@@ -0,0 +1,84 @@
+package org.bcit.comp2522.project;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.bson.Document;
+import processing.core.PApplet;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ScoreboardMenu {
+
+    private PApplet pApplet;
+    DatabaseHandler db;
+    private Button goBackButton;
+
+    private ArrayList<Button> buttons;
+
+    public ScoreboardMenu(PApplet pApplet) {
+        this.pApplet = pApplet;
+        this.buttons = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper();
+        DatabaseHandler.Config config;
+        try {
+            config = mapper.readValue(new File("src/main/java/org/bcit/comp2522/project/config.json"), DatabaseHandler.Config.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        // Use the values from the Config object to create the DatabaseHandler
+        db = new DatabaseHandler(config.getDB_USERNAME(), config.getDB_PASSWORD());
+        goBackButton = new Button("Go Back", pApplet.width / 2, pApplet.height - 50, 100, 50, 20, 0xFFFFFFFF, this::goBackToMainMenu);
+        buttons.add(goBackButton);
+    }
+
+    public void draw() {
+        ArrayList<Document> topScores = db.getTopScores();
+
+        // Clear the screen
+        pApplet.background(0);
+
+        // Draw the title
+        pApplet.textSize(64);
+        pApplet.fill(255);
+        pApplet.textAlign(PApplet.CENTER);
+        pApplet.text("High Scores", pApplet.width / 2, pApplet.height / 8);
+
+        // Draw the highest score
+        pApplet.textSize(32);
+        pApplet.fill(255);
+        pApplet.textAlign(PApplet.CENTER);
+        int y = pApplet.height / 4;
+        if (!topScores.isEmpty()) {
+            Document highestScore = topScores.get(0);
+            int value = highestScore.getInteger("score");
+            pApplet.text(String.format("Highest Score: %d", value), pApplet.width / 2, y);
+            y += 50;
+        } else {
+            System.out.println("empty");
+        }
+
+        // Draw the 2nd to 10th highest scores
+        for (int i = 1; i < Math.min(topScores.size(), 10); i++) {
+            Document score = topScores.get(i);
+            int value = score.getInteger("score");
+            pApplet.text(String.format("%d. %d", i + 1, value), pApplet.width / 2, y);
+            y += 50;
+        }
+
+        goBackButton.draw(pApplet);
+    }
+
+    public void mousePressed() {
+        for (Button button : buttons) {
+            if (button.isMouseOver(pApplet.mouseX, pApplet.mouseY)) {
+                button.onClick();
+                System.out.println("go back buttion clicked ");
+            }
+        }
+    }
+    public void goBackToMainMenu () {
+        LevelManager.getInstance().setState(0); // Set the state to 0 (main menu)
+    }
+
+}
--- src/main/java/org/bcit/comp2522/project/StartMenu.java
+++ src/main/java/org/bcit/comp2522/project/StartMenu.java
@@ -1,42 +1,18 @@
 package org.bcit.comp2522.project;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.bson.Document;
 import processing.core.PApplet;
-
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.function.Consumer;
-
 
 public class StartMenu extends PApplet{
 
   private PApplet pApplet;
   private ArrayList<Button> buttons;
   private boolean buttonsInitialized = false;
-  private Consumer<Integer> onStateChange;
-
-  private Button goBackButton;
-
-  DatabaseHandler db;
-
 
   // MENU SETUP //
-
-  public StartMenu(PApplet pApplet, Consumer<Integer> onStateChange) {
+  public StartMenu(PApplet pApplet) {
     this.pApplet = pApplet;
     buttons = new ArrayList<>();
-    this.onStateChange = onStateChange;
-    ObjectMapper mapper = new ObjectMapper();
-    DatabaseHandler.Config config;
-    try {
-      config = mapper.readValue(new File("src/main/java/org/bcit/comp2522/project/config.json"), DatabaseHandler.Config.class);
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-    // Use the values from the Config object to create the DatabaseHandler
-    db = new DatabaseHandler(config.getDB_USERNAME(), config.getDB_PASSWORD());
   }
 
   public void draw() {
@@ -58,6 +34,7 @@ public class StartMenu extends PApplet{
     for (Button button : buttons) {
       if (button.isMouseOver(pApplet.mouseX, pApplet.mouseY)) {
         button.onClick();
+        System.out.println("start menu buttion clicked ");
       }
     }
   }
@@ -67,74 +44,16 @@ public class StartMenu extends PApplet{
   }
 
   public void startGame() {
-    //TODO: Implement this method to start the game
-    if (onStateChange != null) {
-      onStateChange.accept(1); // Set the state to 1
-    }
+    LevelManager.getInstance().resetGame();
+    LevelManager.getInstance().setState(1);
   }
 
   public void openScoreboard() {
-    if (onStateChange != null) {
-      onStateChange.accept(2); // Set the state to 2 for the Scoreboard
-    }
+    LevelManager.getInstance().setState(2); // Set the state to 0 (main menu)
   }
 
   public void exitGame() {
     pApplet.exit();
-  }
-
-  public void drawScoreboard() {
-    ArrayList<Document> topScores = db.getTopScores();
-
-    // Clear the screen
-    pApplet.background(0);
-
-    // Draw the title
-    pApplet.textSize(64);
-    pApplet.fill(255);
-    pApplet.textAlign(PApplet.CENTER);
-    pApplet.text("High Scores", pApplet.width / 2, pApplet.height / 8);
-
-    // Draw the highest score
-    pApplet.textSize(32);
-    pApplet.fill(255);
-    pApplet.textAlign(PApplet.CENTER);
-    int y = pApplet.height / 4;
-    if (!topScores.isEmpty()) {
-//      pApplet.text("High Scores", pApplet.width , pApplet.height);
-      Document highestScore = topScores.get(0);
-      int value = highestScore.getInteger("score");
-      pApplet.text(String.format("Highest Score: %d", value), pApplet.width / 2, y);
-      y += 50;
-    } else {
-      System.out.println("empty");
-    }
-
-    // Draw the 2nd to 10th highest scores
-    for (int i = 1; i < Math.min(topScores.size(), 10); i++) {
-      Document score = topScores.get(i);
-      int value = score.getInteger("score");
-      pApplet.text(String.format("%d. %d", i + 1, value), pApplet.width / 2, y);
-      y += 50;
-    }
-
-    // Draw the go back button
-    if (goBackButton == null) {
-      goBackButton = new Button("Go Back", pApplet.width / 2, pApplet.height - 50, 100, 50, 20, 0xFFFFFFFF, this::goBackToMainMenu);
-    }
-    goBackButton.draw(pApplet);
-  }
-
-  public void goBackToMainMenu() {
-    if (onStateChange != null) {
-      onStateChange.accept(0); // Set the state to 0 (main menu)
-    }
-  }
-
-  public void mousePressedScoreboard() {
-    if (goBackButton != null && goBackButton.isMouseOver(pApplet.mouseX, pApplet.mouseY)) {
-      goBackButton.onClick();
-    }
   }
 }
 
