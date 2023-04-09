acb33e92a595ff31f0e9084e020902547c4fb680
Helen Liu <mail2helenl@gmail.com>
2023-04-08 20:45:58 -0700

    fixed checkstyle errors in accordance with Google checkstyle

--- src/main/java/org/bcit/comp2522/project/Sprite.java
+++ src/main/java/org/bcit/comp2522/project/Sprite.java
@@ -11,15 +11,6 @@ public abstract class Sprite {
 //  protected Color color;
   protected Window window;
 
-  //constructor with color
-//  public Sprite(int xPos, int yPos, int size, Color color, Window window) {
-//    this.x = xPos;
-//    this.y = yPos;
-//    this.size = size;
-//    this.color = color;
-//    this.window = window;
-//  }
-
   //new constructor without
   public Sprite(int xPos, int yPos, int size, Window window) {
     this.x = xPos;
